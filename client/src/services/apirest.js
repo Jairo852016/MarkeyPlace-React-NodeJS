a0b271069923d9d19164ec729c071482aa1d6631
Jairo Perez <jairo85cd@gmail.com>
2023-07-06 11:48:31 -0500

    npm install products

--- client/src/services/apirest.js
+++ client/src/services/apirest.js
@@ -1,4 +1,4 @@
 export const apiUrl='http://159.223.98.208/api/';
-export const apiUrlP='http://159.223.98.208/api/';
+export const apiUrlP='http://159.223.98.208:3002/api/';
 //export const apiUrl='http://localhost/api/';
 //export const apiUrlP='http://localhost/api/';
