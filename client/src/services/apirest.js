5e5e6c52f28bb33933af9aa375110e5c8b593f5a
Jairo Perez <jairo85cd@gmail.com>
2023-07-06 11:02:44 -0500

    npm install cors

--- client/src/services/apirest.js
+++ client/src/services/apirest.js
@@ -1,4 +1,4 @@
 export const apiUrl='http://159.223.98.208/api/';
 export const apiUrlP='http://159.223.98.208/api/';
-//export const apiUrl='http://localhost:3000/api/';
-//export const apiUrlP='http://localhost:3002/api/';
+//export const apiUrl='http://localhost/api/';
+//export const apiUrlP='http://localhost/api/';
