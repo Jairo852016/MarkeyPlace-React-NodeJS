b24968018602a9948f5d872ebafa5aeaf553153d
Jairo Perez <jairo85cd@gmail.com>
2023-07-05 19:09:48 -0500

    Actualizacion Nginx

--- app/product/index.js
+++ app/product/index.js
@@ -10,14 +10,7 @@ const app=express();
 
 app.use(bodyParser.json());
 
-// Middleware para configurar los encabezados CORS
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
-    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+
 
 //ROUTER
 
